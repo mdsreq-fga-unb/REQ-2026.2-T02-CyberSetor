8e66e124d02e5452b86307a9fa9e3587811b0d73
daniboycam <danielbatista1908112@gmail.com>
2026-09-15 12:49:37 -0300

    feat(dojo): implementa fatia vertical da api de requisitos-dojo

---
+++ api/src/requisitos-dojo/requisitos-dojo.controller.ts
@@ -0,0 +1,19 @@
+// NestJS (Arquivo detalhado - slide D2-S16)
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateRequisitoDojoDto } from './dto/create-requisito-dojo.dto';
+import { RequisitosDojoService } from './requisitos-dojo.service';
+
+@Controller('requisitos-dojo')
+export class RequisitosDojoController {
+  constructor(private readonly requisitos: RequisitosDojoService) {}
+
+  @Get()
+  listar() {
+    return this.requisitos.listar();
+  }
+
+  @Post()
+  criar(@Body() dto: CreateRequisitoDojoDto) {
+    return this.requisitos.criar(dto);
+  }
+}
